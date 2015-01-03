2302cc0865e4169e1451074e2b4933777ead8043
Jordan Jozwiak <jordanjoz1@gmail.com>
2015-01-03 14:46:23 -0800

    use argparse

--- count-views.py
+++ count-views.py
@@ -2,6 +2,7 @@ import flickrapi
 import csv
 import sys
 import datetime
+import argparse
 
 # FILL IN THESE VALUES, AND DON'T CHANGE ANYTHING ELSE
 api_key = u'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
@@ -15,33 +16,33 @@ flickr = flickrapi.FlickrAPI(api_key, api_secret)
 
 MAX_PHOTOS_PER_PAGE = 500
 
-def main(userId, fname=None):
+def main(userId, fname):
 
     # get number of photos for the user
     userInfo = flickr.people.getInfo(user_id=userId)
     count = int(userInfo[0].find('photos').find('count').text)
     pages = count / MAX_PHOTOS_PER_PAGE + 1
     print 'Counting views for %d photos...' % (count)
-    
-	# get list of photos
+
+    # get list of photos
     photo_pages = []
     for page in range(1,pages + 1):
         photo_pages.append(flickr.photos.search(user_id=userId, per_page=str(MAX_PHOTOS_PER_PAGE), page=page))
-	
-	# get view count for each photo
+    
+    # get view count for each photo
     data = []
     for photo_page in photo_pages:
         for photo in photo_page[0]:
             data.append(get_photo_data(photo.get('id')))
-		
-	# write counts to output
-	if (fname is not None):
-		rows = create_rows_from_data(data)
-		write_to_csv(fname, columns, rows)
-		print 'Photo data successfully written to %s (this could take hours if you have hundreds of photos)' % (fname)
-	
-	# display view count for photos
-	print 'Total photo views: %d' % (calc_total_views_from_data(data))
+        
+    # write counts to output
+    if (fname is not None):
+        rows = create_rows_from_data(data)
+        write_to_csv(fname, columns, rows)
+        print 'Photo data successfully written to %s (this could take hours if you have hundreds of photos)' % (fname)
+    
+    # display view count for photos
+    print 'Total photo views: %d' % (calc_total_views_from_data(data))
     
 def calc_total_views_from_data(data):
     total = 0
@@ -68,24 +69,29 @@ def create_rows_from_data(data):
         row=[title,upload_date,str(photo_id),url,description,str(views),str(favs),str(comments)]
         rows.append(row)
     return rows
-	
+    
 def get_photo_data(photo_id):
     info = flickr.photos.getinfo(photo_id=photo_id)
     favs = flickr.photos.getFavorites(photo_id=photo_id)
     return {'info': info, 'favs': favs}
-	
+    
 def write_to_csv(fname, header, rows):
     with open(fname, 'wb') as csvfile:
         csvwriter = csv.writer(csvfile, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
         csvwriter.writerow(header)
         for row in rows:
             csvwriter.writerow([s.encode("utf-8").replace(',', '').replace('\n','') for s in row])
-			
+            
 if __name__ == "__main__":
-	argc = len(sys.argv)
-	if argc == 2:
-		main(sys.argv[1])
-	elif argc == 3:
-		main(sys.argv[1], sys.argv[2])
-	else:
-		sys.exit('Usage: %s <user-id> [output-file-name]' % (sys.argv[0]))
+    # parse arguments and do error checking
+    parser = argparse.ArgumentParser()
+    parser.add_argument('user_id',
+                        help='The id of the user whose total views will be counted.',
+                        default='.')
+    parser.add_argument('--output',
+                        help='Name of the output file',
+                        default=None)
+    args = parser.parse_args()
+    
+    # run main
+    main(args.user_id, args.output)
